84443d1680f387f62c108b6a93475a6f7f317080
Brandon Martin-Anderson <badhill@gmail.com>
2020-01-30 07:41:41 +0000

    setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="osmpath",
+    version="0.1.0",
+
+    author="Brandon Martin-Anderson",
+    author_email="badhill@gmail.com",
+
+    description="Find elevation profiles of linestrings.",
+
+    packages=setuptools.find_packages(),
+
+    install_requires=["rasterio","numpy","scipy","shapely"],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
